25cfac8374b2ab7a16c3155fe955de395cb3e420
Shinpachi1009 <jamaricjes@gmail.com>
2025-07-30 15:07:34 +0800

    sample website

---
+++ php/demo/sample website/views/link.view.php
@@ -0,0 +1,9 @@
+<?php require ('common/head.php'); ?>
+    <?php require ('common/nav.php') ?>
+    <?php require ('common/banner.php'); ?>
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            this is link page
+        </div>
+    </main>
+<?php require ('common/foot.php') ?>
